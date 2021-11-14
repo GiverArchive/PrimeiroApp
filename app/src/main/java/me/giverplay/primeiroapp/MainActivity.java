d5f86fc60a953ff36b4dcf210fa7e9328e66e665
GiverPlay007 <play.giver.007@gmail.com>
2021-11-14 18:14:32 -0300

    Ações

--- app/src/main/java/me/giverplay/primeiroapp/MainActivity.java
+++ app/src/main/java/me/giverplay/primeiroapp/MainActivity.java
@@ -3,6 +3,8 @@ package me.giverplay.primeiroapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void alterarTexto(View view) {
+        TextView texto = findViewById(R.id.textoExibido);
+        texto.setText("Clicou mesmo hehe");
+    }
+
 }
